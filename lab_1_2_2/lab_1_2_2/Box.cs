66deab79023a75af9c3794fcc4ce52149dd15e99
rudyxx <madwasp@ukr.net>
2016-02-15 02:01:05 +0200

    1_2_2

---
+++ lab_1_2_2/lab_1_2_2/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_2_2
+{
+    class Box
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public char symbol { get; set; }
+
+        long multiplier;
+        string mess;
+
+        public string message {
+            get 
+            {
+                return mess;
+            }
+            set 
+            { 
+                string temp = value;
+                if (temp.Length > width)
+                {
+                    mess = temp.Substring(0, width - 2);
+                }
+                else
+                    mess = value;
+            }
+        }
+
+        
+        public void Draw()
+        {
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i < height+y; i++)
+            {
+                for (int j = 0; j < width+x; j++)
+                {
+                    if ((i == y && j >= x) || (i == y + height - 1 && j >= x) || (i >= y && j == x + width - 1) || (i >= y && j == x ))
+                    {
+                            Console.Write(symbol);                       
+                    }
+                    else if((i == y+(height / 2)) && (j == x+ ((width / 2) - (message.Length/2))))
+                    {
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        j += message.Length-1;
+                        for (int k = 0; k < message.Length; k++)
+                        {
+                            Console.Write(message[k]);
+                        }
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                    }
+                    else
+                        Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void WriteMessage()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            multiplier = Math.BigMul(width, height);
+            Console.WriteLine("Math.BigMul(width, height) = {0}", multiplier);
+            
+        }
+
+    }
+}
---
+++ lab_1_2_2/lab_1_2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box b = new Box();
+            //let's skip the boring input
+            b.x = 10;
+            b.y = 10;
+            b.height = 16;
+            b.width = 20;
+            b.symbol = '*';
+            b.message = "Hello! Let me introduce myself!  My name is... oh God,why am I fooling myself, who cares about my name?";
+            b.Draw();
+
+
+            b.WriteMessage();
+            Console.ReadKey();
+        }
+    }
+}
