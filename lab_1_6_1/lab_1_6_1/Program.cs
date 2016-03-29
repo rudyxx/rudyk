d86ffc163843c02c8f872e02d2b86d598f9d31c8
rudyxx <madwasp@ukr.net>
2016-03-29 14:24:16 +0300

    I do not quite understand the task, so probably I did something wrong

---
+++ lab_1_6_1/lab_1_6_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_1_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadManipulator tManipulator = new ThreadManipulator();
+            int[] arr = { 5, 15 };
+
+            Thread thread_for_AddingOne1 = new Thread(tManipulator.AddingOne);
+            Thread thread_for_AddingOne2 = new Thread(tManipulator.AddingOne);
+            Thread thread_for_AddingCustom = new Thread(tManipulator.AddingCustomValue);
+            Thread thread_for_Stop = new Thread(tManipulator.Stop);
+
+
+            thread_for_Stop.IsBackground = true;
+
+            thread_for_AddingOne1.Start(10);
+            thread_for_AddingOne2.Start(20);
+            thread_for_AddingCustom.Start(arr);
+            thread_for_Stop.Start();
+
+            thread_for_AddingOne1.Join();
+            thread_for_AddingOne2.Join();
+            thread_for_AddingCustom.Join();
+            thread_for_Stop.Join();
+
+        }
+    }
+}
---
+++ lab_1_6_1/lab_1_6_1/ThreadManipulator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_1_6_1
+{
+    class ThreadManipulator
+    {
+        ConsoleKey key;
+        object block = new object();
+
+        public void AddingOne(object obj)
+        {
+                Monitor.Enter(block);
+                int counter = 1;
+                int tempNum = (int) obj;
+                while (counter <= 100 && key != ConsoleKey.Q)
+                {
+                    if (counter % tempNum == 0)
+                        Console.WriteLine("AddingOne Divisible by {0}, courrent value {1}", tempNum, counter);
+                    counter++;
+                    Thread.Sleep(250);
+                }
+                Console.WriteLine("thread is ended");
+                Monitor.Exit(block);
+            
+
+        }
+
+        public void AddingCustomValue(object obj)
+        {
+            int [] tempNum = (int[])obj;
+            int divisibleBy = tempNum[0];
+            int step = tempNum[1];
+
+            int counter = 1;
+            while (counter <= 100 && key != ConsoleKey.W)
+            {
+                if ((counter % step) == 0)
+                {
+                    Console.WriteLine("Divisible by {0}, courrent value {1}", divisibleBy, counter );                    
+                }
+                Thread.Sleep(250);
+                counter++;
+            }
+                 Console.WriteLine("AddingCustomValue thread is ended");
+        }
+
+        public void Stop()
+        {
+            while(key != ConsoleKey.E)
+            {
+                key = Console.ReadKey().Key;
+                Console.WriteLine("Stop() thread is ended");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
